8cc375df9480cf6b31f0226e050c42c6fed26f37
Hassan Rasooli <hassan.rasooli360@gmail.com>
2022-12-31 12:30:15 +0330

    change class components to function components

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,20 +1,17 @@
-import { Component } from "react";
 import './card.style.css'
 
-class Card extends Component {
-    render() {
-        const { id, firstName, maidenName, image } = this.props.monster;
+const Card = ({ monster }) => {
+    const { id, firstName, maidenName, image } = monster;
 
-        return (
-            <div className="card-container" key={id}>
-                <img
-                    alt={`monster ${firstName}`}
-                    src={image}
-                />
-                <h2>{maidenName}</h2>
-            </div>
-        )
-    }
+    return (
+        <div className="card-container" key={id}>
+            <img
+                alt={`monster ${firstName}`}
+                src={image}
+            />
+            <h2>{maidenName}</h2>
+        </div>
+    )
 }
 
 
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,16 +1,13 @@
-import { Component } from "react";
 import './search-box.style.css'
-class SearchBox extends Component {
-    render() {
+const SearchBox =({className,placeholder,onChangeHandler})=>{
         return (
             <input
                 type='search'
-                className={`search-box ${this.props.className}`}
-                placeholder={this.props.placeholder}
-                onChange={this.props.onChangeHandler}
+                className={`search-box ${className}`}
+                placeholder={placeholder}
+                onChange={onChangeHandler}
             />
         )
-    }
 }
 
 export default SearchBox;
